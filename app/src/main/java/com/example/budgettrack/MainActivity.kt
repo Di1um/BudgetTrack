bfe163c942ca129fc294235139d7ca7a9a8881d7
Di1um <dilumayeshan10@gmail.com>
2023-05-21 19:53:49 +0530

    welcome screen button & menu icon to toolbar change, strings added, added files menu.xml, menu_icon v 1.0020

--- app/src/main/java/com/example/budgettrack/MainActivity.kt
+++ app/src/main/java/com/example/budgettrack/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.budgettrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_screen_one)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 }
